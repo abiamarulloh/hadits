93e26454d04ec3daaf269943c77f7c087bb2f14f
abi amarulloh <abiamarulloh06@gmail.com>
2023-10-05 11:55:13 +0700

    fchore: change api endpoint route

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = () => {
         source: "/api/:path*",
         // destination: "http://localhost:3000/api/:path*", // for local
         // destination: "https://hadist-app-backend.herokuapp.com/api/:path*", // for prod heroku
-        destination: "https://api.hadits.amarabi.my.id/api/:path*", // for prod amarabi.com
+        destination: "https://api-hadits.amarabi.my.id/api/:path*", // for prod amarabi.com
       },
     ];
   };
